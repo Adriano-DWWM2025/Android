83fb72692bd4d75fae9e0740241b309f9ebd906d
Adriano <03.dwwm2025@gmail.com>
2025-04-02 16:39:44 +0200

    update MenuBurger

--- MenuBurger/app/src/main/java/com/example/menuburger/MainActivity.java
+++ MenuBurger/app/src/main/java/com/example/menuburger/MainActivity.java
@@ -5,9 +5,11 @@ import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -33,11 +35,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
         navigationView.setNavigationItemSelectedListener(this);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        return false;
+        if (item.getItemId() == R.id.nav_home)
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new HomeFragment())
+                    .commit();
+
+        if (item.getItemId() == R.id.nav_profile)
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new ProfileFragment())
+                    .commit();
+
+        if (item.getItemId() == R.id.nav_messages)
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_destination_activityMain, new MessagesFragment())
+                    .commit();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
     }
+
 }
--- MenuBurger/app/src/main/java/com/example/menuburger/MessagesFragment.java
+++ MenuBurger/app/src/main/java/com/example/menuburger/MessagesFragment.java
@@ -7,14 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 public class MessagesFragment extends Fragment {
+
+    private TextView txtView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+        View monView = inflater.inflate(R.layout.fragment_messages, container, false);
+        txtView = txtView.findViewById(R.id.txtView);
+        return monView;
+
     }
 }
